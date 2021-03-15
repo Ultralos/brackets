0a5d543f9bed5ad6942baa676596b71fb66bbcb1
Yusuf Giyaziddinov <ygiyaziddinov@gmail.com>
2021-03-15 13:14:12 +0500

    feat: Task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports =  function check(str, bracketsConfig) {
+    let catchError = 0;
+    let emptyBox = [];
+
+    let OpeningBrackets = bracketsConfig.map((each) => {
+        return each.join("").substr(0, 1);
+    });
+
+    let ClosingBrackets = bracketsConfig.map((each) => {
+        return each.join("").substr(1, 2);
+    });
+
+    let strSplited = str.split("");
+
+    for (let i = 0; i < strSplited.length; i++) {
+        let item = strSplited[i];
+        let isOpening = OpeningBrackets.indexOf(item);
+
+        if (isOpening !== -1) {
+            if (
+                ClosingBrackets.indexOf(item) === isOpening &&
+                emptyBox[emptyBox.length - 1] === item
+            ) {
+                emptyBox.pop();
+                continue;
+            }
+            emptyBox.push(item);
+            continue;
+        }
+
+        let lastOpened = emptyBox[emptyBox.length - 1];
+        if (
+            OpeningBrackets.indexOf(lastOpened) ===
+            ClosingBrackets.indexOf(item)
+        ) {
+            emptyBox.pop();
+            continue;
+        } else {
+            catchError += 1;
+            break;
+        }
+    }
+    if(catchError === 0 && emptyBox.length === 0){
+        return true
+    }else{
+        return false
+    }
 }
+
